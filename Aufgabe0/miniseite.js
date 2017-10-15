e9cc81cd71189438886744db0ffe8bc9c3ee8d6d
Craiso <32641068+Craiso@users.noreply.github.com>
2017-10-15 15:07:47 +0200

    Add files via upload

---
+++ Aufgabe0/miniseite.js
@@ -0,0 +1,20 @@
+/*
+Aufgabe 00: Miniseite
+Name: Chris Schmieder
+Matrikel: 256029
+Datum: 15.10.2017
+        
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe.
+Er wurde nicht kopiert und auch nicht diktiert. */
+var name = prompt("Mein Name ist...");
+function init() {
+    var node = document.getElementById("gruss");
+    if (name.length < 1) {
+        node.innerHTML = "Schade drum :(";
+    }
+    if (name.length >= 1) {
+        node.innerHTML = "Herzlich willkommen liebe/-r " + name + " auf meiner Miniseite.";
+    }
+}
+window.addEventListener("load", init);
+//# sourceMappingURL=miniseite.js.map
